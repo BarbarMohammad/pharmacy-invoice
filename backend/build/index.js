88479adf8c54a2c31d498111d5e7bbc2e780c137
Md Barbar <dev@DEVs-MacBook-Air.local>
2025-08-19 09:32:43 -0400

    invoice

---
+++ backend/build/index.js
@@ -0,0 +1,45 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const multer_1 = __importDefault(require("multer"));
+const cors_1 = __importDefault(require("cors"));
+const axios_1 = __importDefault(require("axios"));
+const xlsx_1 = __importDefault(require("xlsx"));
+const discrepancyChecker_1 = require("./utils/discrepancyChecker");
+const fs_1 = __importDefault(require("fs"));
+console.log("Starting backend...");
+// Ensure uploads directory exists at runtime
+if (!fs_1.default.existsSync('uploads')) {
+    fs_1.default.mkdirSync('uploads');
+}
+const app = (0, express_1.default)();
+app.use((0, cors_1.default)({
+    origin: ['https://pharmacy-frontend-umber.vercel.app','http://localhost:3000'],
+    methods: ['GET', 'POST', 'OPTIONS'],
+    allowedHeaders: ['Content-Type']
+}));
+const upload = (0, multer_1.default)({ dest: 'uploads/' });
+app.post('/api/upload', upload.single('invoice'), async (req, res) => {
+    try {
+        if (!req.file) {
+            return res.status(400).json({ error: 'No file uploaded' });
+        }
+        const filePath = req.file.path;
+        const workbook = xlsx_1.default.readFile(filePath);
+        const sheet = workbook.Sheets[workbook.SheetNames[0]];
+        const invoiceRecords = xlsx_1.default.utils.sheet_to_json(sheet, { header: 1 });
+        const { data: refDrugs } = await axios_1.default.get('https://685daed17b57aebd2af6da54.mockapi.io/api/v1/drugs');
+        const discrepancies = (0, discrepancyChecker_1.checkDiscrepancies)(invoiceRecords, refDrugs);
+        res.json({ discrepancies });
+    }
+    catch (err) {
+        res.status(500).json({ error: 'Processing failed', details: err });
+    }
+});
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+    console.log(`Backend running on port ${PORT}`);
+});
